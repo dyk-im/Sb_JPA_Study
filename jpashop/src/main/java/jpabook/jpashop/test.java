96e38d4079be2548b312d40db07ecb5ad7d70d93
Dongyun Kim <djskaldrjfj@naver.com>
2024-10-09 02:40:58 +0900

    first

---
+++ jpashop/src/main/java/jpabook/jpashop/test.java
@@ -0,0 +1,12 @@
+package jpabook.jpashop;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class test {
+
+	private String data;
+}
